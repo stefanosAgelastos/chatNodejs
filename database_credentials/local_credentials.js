fe4b0715142d0e2ed9f34b24e94e7bf6f5bcdbb6
Stefanos <sagelastos@gmail.com>
2018-06-05 13:31:27 +0200

    small changes in credentials

--- database_credentials/local_credentials.js
+++ database_credentials/local_credentials.js
@@ -1,4 +1,10 @@
 /* all taken from docker environment variables */
+/* exports.host = "localhost";
+exports.database = "chat";
+exports.user = "root";
+ */
+
+
 exports.host = process.env.DB_HOST;
 exports.database = process.env.DB_NAME;
 exports.user = process.env.DB_USER;
