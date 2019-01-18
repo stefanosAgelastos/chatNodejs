d006a9e851fe53b6bd18bc0274b6c5dca2e3e003
Stefanos <sagelastos@gmail.com>
2019-01-18 20:42:18 +0100

    fixed knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,7 @@
-var credentials = require("./database_credentials/local_credentials.js");
-
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || { user: 'me', database: 'my_app' }
+    connection: process.env.DATABASE_URL
   }
-
 };
